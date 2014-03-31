5149df5196bb939db79fb9525519107c5221919f
Francesco Dalprà <jabba.dev@gmail.com>
2014-03-31 16:10:40 +0200

    Some byte error

--- decipher.js
+++ decipher.js
@@ -9,15 +9,9 @@ var crypto = require('crypto'),
 process.stdin.on('data', function(chunk) {
 	var chunkB64 = deipher.update(chunk.toString(),'base64','base64')
 	outStream.write(new Buffer(chunkB64,'base64'));
-	//outStream.write(deipher.update(chunk,'base64','base64'));
-	//outStream.write(chunk);
 });
 
 process.stdin.on('end', function() {
-  //msg = deipher.final('binary');
-  //var chunkB64 = deipher.final('base64');
   outStream.write(new Buffer(deipher.final('base64'),'base64'));
-  //outStream.write(chunkB64);
-  //outStream.write(deipher.final('binary'));
   outStream.close();
 });
