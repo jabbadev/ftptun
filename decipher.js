4c6532a4146d5ba236069692c94c006cea2a4a18
Francesco Dalprà <jabba.dev@gmail.com>
2014-04-02 12:39:19 +0200

    WIP

--- decipher.js
+++ decipher.js
@@ -3,16 +3,25 @@
 var crypto = require('crypto'),
     deipher = crypto.createDecipher('aes-256-cbc',"secret1234")
 	fs = require('fs'),
-	outStream = fs.createWriteStream('output.bin',{ encoding: "binary" }),
+	outStream = fs.createWriteStream('output_decipher.bin',{ encoding: "binary" }),
 	msg = "";
+	
+var inf = fs.createReadStream('orig_file_cipher.tar.gz');
 
-process.stdin.on('data', function(chunk) {
+outStream.on('finish',function(){
+	outStream.close();
+});
+
+//process.stdin.on('data', function(chunk) {
+inf.on('data', function(chunk) {
 	var chunkB64 = deipher.update(chunk);
 	outStream.write(new Buffer(chunkB64,'base64'));
 });
 
-process.stdin.on('end', function() {
+
+
+//process.stdin.on('end', function() {
+inf.on('end', function() {
   var chunkB64 = deipher.final();
-  outStream.write(new Buffer(chunkB64,'base64'));
-  outStream.close();
+  outStream.end(new Buffer(chunkB64,'base64'));
 });
