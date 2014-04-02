be0104bac69f8281ebd6a7e2522bb12ca3480d28
Francesco Dalprà <jabba.dev@gmail.com>
2014-04-02 14:11:28 +0200

    decipher on file or stdout

--- decipher.js
+++ decipher.js
@@ -3,23 +3,23 @@
 var crypto = require('crypto'),
     deipher = crypto.createDecipher('aes-256-cbc',"secret1234")
 	fs = require('fs'),
-	outStream = fs.createWriteStream('output_decipher.bin',{ encoding: "binary" }),
-	msg = "";
+	outStream = ((process.argv[2]) && fs.createWriteStream(process.argv[2]))||process.stdout;
 	
-//var inf = fs.createReadStream('orig_file_cipher.tar.gz');
-
 outStream.on('finish',function(){
 	outStream.close();
 });
 
 process.stdin.on('data', function(chunk) {
-//inf.on('data', function(chunk) {
 	var chunkB64 = deipher.update(chunk);
 	outStream.write(new Buffer(chunkB64,'base64'));
 });
 
 process.stdin.on('end', function() {
-//inf.on('end', function() {
   var chunkB64 = deipher.final();
-  outStream.end(new Buffer(chunkB64,'base64'));
+  if ( process.argv[2] ){
+	outStream.end(new Buffer(chunkB64,'base64'));
+  }
+  else {
+	outStream.write(new Buffer(chunkB64,'base64'));
+  }
 });
