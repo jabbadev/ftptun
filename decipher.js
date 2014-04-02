920b9ce9e95e90d4c25ba3621c0d6d61bc0a12e6
Francesco Dalprà <jabba.dev@gmail.com>
2014-04-02 12:44:05 +0200

    First positive test

--- decipher.js
+++ decipher.js
@@ -6,22 +6,20 @@ var crypto = require('crypto'),
 	outStream = fs.createWriteStream('output_decipher.bin',{ encoding: "binary" }),
 	msg = "";
 	
-var inf = fs.createReadStream('orig_file_cipher.tar.gz');
+//var inf = fs.createReadStream('orig_file_cipher.tar.gz');
 
 outStream.on('finish',function(){
 	outStream.close();
 });
 
-//process.stdin.on('data', function(chunk) {
-inf.on('data', function(chunk) {
+process.stdin.on('data', function(chunk) {
+//inf.on('data', function(chunk) {
 	var chunkB64 = deipher.update(chunk);
 	outStream.write(new Buffer(chunkB64,'base64'));
 });
 
-
-
-//process.stdin.on('end', function() {
-inf.on('end', function() {
+process.stdin.on('end', function() {
+//inf.on('end', function() {
   var chunkB64 = deipher.final();
   outStream.end(new Buffer(chunkB64,'base64'));
 });
